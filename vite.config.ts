a8574f64bd04581b399e4f65f1e447a22ca00696
Alem Mohamed <126617660+moumouh6@users.noreply.github.com>
2025-05-15 04:40:44 +0100

    Updated package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
   },
+  preview: {
+    port: 5173,
+    strictPort: true,
+  }
 });
