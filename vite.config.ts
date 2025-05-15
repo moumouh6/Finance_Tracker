5b66b5128da849842d361763861ed8e5279cf7be
Alem Mohamed <126617660+moumouh6@users.noreply.github.com>
2025-05-15 04:44:30 +0100

    Fix preview and rendering issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 5173,
     strictPort: true,
   },
   preview: {
+    host: '0.0.0.0',
     port: 5173,
     strictPort: true,
   }
